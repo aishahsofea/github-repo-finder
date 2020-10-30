317bec4648e46f03aafb7483622cb26a278fd4e8
aishahsofea <aishahsofea22@gmail.com>
2020-10-30 14:10:42 +0800

    Fixed export in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { RepoFinder } from "./RepoFinder";
 import Typography from "@material-ui/core/Typography";
 
-export class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <div className="App" style={{ textAlign: "center" }}>
